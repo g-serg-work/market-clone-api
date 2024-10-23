85aead53848acc483501ff441a1447e29368f1c6
g-serg-work <gor.serg.work77@gmail.com>
2024-10-23 16:37:37 +0300

    prettier add

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const fs = require("fs");
-const jsonServer = require("json-server");
-const path = require("path");
-const http = require("http");
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+const http = require('http');
 
 // custom
-const marketDataParse = require("./market/reader.js");
-const cfg = require("./market/cfg.json");
-const loginPost = require("./handler/login");
-const profileGet = require("./handler/profileGet");
-const catalogGet = require("./market/handler/catalogGet");
-const favoriteCategoryGet = require("./market/handler/favoriteCategoryByUserGet.js");
-const ordersByUserGet = require("./market/handler/ordersByUserGet");
-const authorizationCheck = require("./handler/authorizationCheck");
+const marketDataParse = require('./market/reader.js');
+const cfg = require('./market/cfg.json');
+const loginPost = require('./handler/login');
+const profileGet = require('./handler/profileGet');
+const catalogGet = require('./market/handler/catalogGet');
+const favoriteCategoryGet = require('./market/handler/favoriteCategoryByUserGet.js');
+const ordersByUserGet = require('./market/handler/ordersByUserGet');
+const authorizationCheck = require('./handler/authorizationCheck');
 //
 
-const dir = path.resolve(__dirname, "./market/data");
+const dir = path.resolve(__dirname, './market/data');
 const marketDb = marketDataParse(dir);
 
-const mainDbPath = path.resolve(__dirname, "db.json");
-const mainDbGetter = () => JSON.parse(fs.readFileSync(mainDbPath, "UTF-8"));
+const mainDbPath = path.resolve(__dirname, 'db.json');
+const mainDbGetter = () => JSON.parse(fs.readFileSync(mainDbPath, 'UTF-8'));
 
 const handlerCfg = { cfg, mainDbGetter, marketDb };
 
@@ -31,25 +31,25 @@ const server = jsonServer.create();
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
-const badRequest = (_, res) => res.status(400).json({ message: "Bad Request" });
+const badRequest = (_, res) => res.status(400).json({ message: 'Bad Request' });
 
 // free
-server.get("/catalog/:catalogId", catalogGet(handlerCfg));
+server.get('/catalog/:catalogId', catalogGet(handlerCfg));
 // private
-server.post("/login", loginPost(handlerCfg));
-server.get("/profile", authorizationCheck(handlerCfg), profileGet(handlerCfg));
+server.post('/login', loginPost(handlerCfg));
+server.get('/profile', authorizationCheck(handlerCfg), profileGet(handlerCfg));
 server.get(
-    "/favorite-category",
+    '/favorite-category',
     authorizationCheck(handlerCfg),
     favoriteCategoryGet(handlerCfg),
 );
 server.get(
-    "/orders",
+    '/orders',
     authorizationCheck(handlerCfg),
     ordersByUserGet(handlerCfg),
 );
 //not found
-server.use("*", badRequest);
+server.use('*', badRequest);
 
 // run server
 const HTTP_PORT = 8000;
